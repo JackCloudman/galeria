f0897e6d9f93fa60ca376b13e3307e603744f0e0
jackcloudman <maylo360xd@gmail.com>
2018-08-18 22:38:34 -0500

    Pagina de inciio

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Foto
+def home(request):
+    fotos = Foto.objects.order_by('fecha')
+    return render(request, 'index.html', {'fotos':fotos})
 
 # Create your views here.
