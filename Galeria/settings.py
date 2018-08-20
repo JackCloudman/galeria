8f1c3cdc6e0a369ae6b413fdf468b86cd0a1f5f5
jackcloudman <maylo360xd@gmail.com>
2018-08-19 20:49:42 -0500

    Primera version

--- Galeria/settings.py
+++ Galeria/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/jack/galeria/static/'
-
+FILE_UPLOAD_PERMISSIONS = 0o644
 
 
 
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Foto
 def home(request):
-    fotos = Foto.objects.order_by('fecha')
+    fotos = Foto.objects.order_by('-fecha')[:5]
     return render(request, 'index.html', {'fotos':fotos})
 def galeria(request):
-    fotos = Foto.objects.order_by('fecha')
+    fotos = Foto.objects.order_by('-fecha')
     return render(request, 'galeria.html', {'fotos':fotos})
 
 # Create your views here.
